838cbab1c0879cde20d2c778ac0101be0d482a02
Andres <aeum3893@gmail.com>
2025-03-25 08:52:11 -0500

    Enhance WhatsApp client initialization with additional parameters for improved logging and message handling

--- app/services/whatsapp_service.rb
+++ app/services/whatsapp_service.rb
@@ -2,7 +2,8 @@ class WhatsappService
   def self.send_message(wa_phone_number_id, recipient_phone_number, message)
     # client = WhatsappSdk::Api::Messages.new
     wa_integration = WaIntegration.find_by(phone_number_id: wa_phone_number_id)
-    client = WhatsappSdk::Api::Client.new(wa_integration.access_token)
+
+    client = WhatsappSdk::Api::Client.new(wa_integration.access_token, 22, Logger.new(STDOUT), { bodies: true })
     client.messages.send_text(
       sender_id: wa_phone_number_id.to_i,
       recipient_number: recipient_phone_number.to_i,

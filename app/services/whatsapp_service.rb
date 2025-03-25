a14f0192860ca3515d0dc854d6b2649e19b3b50b
Andres <aeum3893@gmail.com>
2025-03-25 08:43:53 -0500

    Use user's access token instead of mine for sending messages

--- app/services/whatsapp_service.rb
+++ app/services/whatsapp_service.rb
@@ -1,6 +1,8 @@
 class WhatsappService
   def self.send_message(wa_phone_number_id, recipient_phone_number, message)
-    client = WhatsappSdk::Api::Messages.new
+    # client = WhatsappSdk::Api::Messages.new
+    wa_integration = WaIntegration.find_by(phone_number_id: wa_phone_number_id)
+    client = WhatsappSdk::Api::Client.new(wa_integration.access_token)
     client.send_text(
       sender_id: wa_phone_number_id.to_i,
       recipient_number: recipient_phone_number.to_i,

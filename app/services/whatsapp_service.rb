000fcd25c59c028fac32a9a302acc2c1c3e10f69
Andres <aeum3893@gmail.com>
2025-03-25 08:55:42 -0500

    no api version

--- app/services/whatsapp_service.rb
+++ app/services/whatsapp_service.rb
@@ -3,7 +3,7 @@ class WhatsappService
     # client = WhatsappSdk::Api::Messages.new
     wa_integration = WaIntegration.find_by(phone_number_id: wa_phone_number_id)
 
-    client = WhatsappSdk::Api::Client.new(wa_integration.access_token, 22, Logger.new(STDOUT), { bodies: true })
+    client = WhatsappSdk::Api::Client.new(wa_integration.access_token, Logger.new(STDOUT), { bodies: true })
     client.messages.send_text(
       sender_id: wa_phone_number_id.to_i,
       recipient_number: recipient_phone_number.to_i,

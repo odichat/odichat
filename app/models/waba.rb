f9b1039dcedd1317722a0ab579c7c627916ce660
Andres <aeum3893@gmail.com>
2025-04-07 11:16:11 -0500

    Add business category selection and improve websites input in Waba form

--- app/models/waba.rb
+++ app/models/waba.rb
@@ -3,6 +3,32 @@ class Waba < ApplicationRecord
 
   belongs_to :chatbot
 
+  def self.available_verticals
+    {
+      "ALCOHOL" => "Alcoholic Beverages",
+      "APPAREL" => "Clothing and Apparel",
+      "AUTO" => "Automotive",
+      "BEAUTY" => "Beauty, Spa and Salon",
+      "EDU" => "Education",
+      "ENTERTAIN" => "Entertainment",
+      "EVENT_PLAN" => "Event Planning and Service",
+      "FINANCE" => "Finance and Banking",
+      "GOVT" => "Public Service",
+      "GROCERY" => "Food and Grocery",
+      "HEALTH" => "Medical and Health",
+      "HOTEL" => "Hotel and Lodging",
+      "NONPROFIT" => "Non-profit",
+      "ONLINE_GAMBLING" => "Online Gambling & Gaming",
+      "OTC_DRUGS" => "Over-the-Counter Drugs",
+      "OTHER" => "Other",
+      "PHYSICAL_GAMBLING" => "Non-Online Gambling & Gaming (E.g. Brick and mortar)",
+      "PROF_SERVICES" => "Professional Services",
+      "RESTAURANT" => "Restaurant",
+      "RETAIL" => "Shopping and Retail",
+      "TRAVEL" => "Travel and Transportation"
+    }
+  end
+
   def get_business_profile
     client = whatsapp_client(self.access_token)
     client.business_profiles.get(self.phone_number_id)
@@ -10,8 +36,14 @@ class Waba < ApplicationRecord
 
   def update_business_profile(params)
     client = whatsapp_client(self.access_token)
+    if params[:websites].present?
+      params[:websites] = params[:websites].split(",").map(&:strip)
+    end
     # business_profiles.update responds with Boolean
-    client.business_profiles.update(phone_number_id: self.phone_number_id, params: params)
+    client.business_profiles.update(
+      phone_number_id: self.phone_number_id,
+      params: params
+    )
   end
 
   def get_connected_phone_number

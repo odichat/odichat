0f335a61b899fd7e255ad74096771d167838a8d7
Andres <aeum3893@gmail.com>
2025-04-25 14:55:39 -0500

    pay + odichat.app address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,20 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/initializers/pay.rb
+++ config/initializers/pay.rb
@@ -1,13 +1,13 @@
 Pay.setup do |config|
   config.business_name = "Odichat"
-  config.business_address = "9898 Colonnade Blvd, San Antonio, TX 78230"
-  config.application_name = "odichat"
-  config.support_email = "aeum3893@gmail.com"
+  config.business_address = "San Antonio, TX 78230\nUSA"
+  config.application_name = "Odichat"
+  config.support_email = "Odichat <odichat.app@gmail.com>"
 
   config.default_product_name = "default"
   config.default_plan_name = "default"
 
   config.enabled_processors = [ :stripe ]
 
-  config.send_emails = false
+  config.send_emails = true
 end

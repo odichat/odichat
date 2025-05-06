97fb933a03dd900507b9ac7516611b6e8244f595
Andres <aeum3893@gmail.com>
2025-05-06 11:13:33 -0500

    fix: typo in devise after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   # Devise
   # **************************************************
   def after_sign_in_path_for(resource)
-    if Feature.enabled?(:paywall)
+    if Flipper.enabled?(:paywall)
       if current_user&.payment_processor&.subscribed?
         chatbots_path
       else

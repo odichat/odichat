b2a4804ae1db9e6a83c78dbad99599582a30ddc0
Andres <aeum3893@gmail.com>
2025-05-14 19:56:24 -0500

    account update params method from app controller to reg controller devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
 
   include Pundit::Authorization
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -15,14 +13,6 @@ class ApplicationController < ActionController::Base
   private
 
   protected
-
-  # **************************************************
-  # Devise
-  # **************************************************
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :city, :country ])
-  end
 
   # **************************************************
   # Error handling

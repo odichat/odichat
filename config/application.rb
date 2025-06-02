5a054f6f1871f0e7dbffc11ff2374cf1402c4707
Andres <aeum3893@gmail.com>
2025-06-02 13:18:59 -0500

    apply translations in es.yml

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,12 @@ module Odisea
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
+
+    # Configure available locales
+    config.i18n.available_locales = [ :en, :es ]
+    # Set default locale
+    config.i18n.default_locale = :es
+
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.

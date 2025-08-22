33edfc3103bf45bc9aeab3879ff6f47dea54debb
Andres <aeum3893@gmail.com>
2025-08-22 12:16:27 -0500

    feat: Update database schema and seed data for new model and status index

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,13 @@
 #   end
 
 if Rails.env.development?
-  # Create a test user
-  user = User.find_or_create_by!(email: 'aeum3893@gmail.com') do |u|
-    u.password = '123456'
-    u.password_confirmation = '123456'
+  # Models
+  models = [ "gpt-4.1-nano", "gpt-4o-mini" ]
+  models.each do |model_name|
+    Model.find_or_create_by!(name: model_name, provider: "openai")
   end
 
-  # Create a model
-  Model.find_or_create_by!(name: "gpt-4o-mini", provider: "openai")
-  Model.find_or_create_by!(name: "gpt-4o", provider: "openai")
-
-  # Create a chatbot for the user
-  # Chatbot.find_or_create_by!(assistant_id: 'asst_AFGyRQRz0BgEOW0kmjFg9wsg') do |chatbot|
-  #   chatbot.name = 'My Assistant'
-  #   chatbot.user = user
-  #   chatbot.model_id = Model.find_by(name: "gpt-4o-mini")
-  # end
+  User.create!(
+    email: "admin@odichat.app", password: "123456"
+  )
 end

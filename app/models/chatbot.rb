a385a874aa37f564cdaa71de201c9ce904b1f5e0
Andres <aeum3893@gmail.com>
2025-08-27 11:08:28 -0500

    fix: change inbox and channel associations to synchronous destruction

--- app/models/chatbot.rb
+++ app/models/chatbot.rb
@@ -5,13 +5,13 @@ class Chatbot < ApplicationRecord
   has_one :vector_store, dependent: :destroy
   has_one :shareable_link, dependent: :destroy
 
-  has_many :inboxes, dependent: :destroy_async
+  has_many :inboxes, dependent: :destroy
   has_many :chats, dependent: :destroy
-  has_many :contacts, dependent: :destroy_async
-  has_many :documents, dependent: :destroy_async
-  has_many :playground_channels, dependent: :destroy_async, class_name: "Channel::Playground"
-  has_many :public_playground_channels, dependent: :destroy_async, class_name: "Channel::PublicPlayground"
-  has_many :whatsapp_channels, dependent: :destroy_async, class_name: "Channel::Whatsapp"
+  has_many :contacts, dependent: :destroy
+  has_many :documents, dependent: :destroy
+  has_many :playground_channels, dependent: :destroy, class_name: "Channel::Playground"
+  has_many :public_playground_channels, dependent: :destroy, class_name: "Channel::PublicPlayground"
+  has_many :whatsapp_channels, dependent: :destroy, class_name: "Channel::Whatsapp"
 
   validates :model_id, presence: true
   validates :temperature, presence: true
